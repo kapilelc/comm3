12f5a7f15f46dfa6dbe2842c83cdf6e10f2b3f54
kapilelc <58108513+kapilelc@users.noreply.github.com>
2025-05-23 18:09:59 -0400

    Update ProductList.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,7 +3,7 @@ import type { Product } from '../types/product';
 
 export const ProductList: React.FC<{ products: Product[] }> = ({ products }) => {
   return (
-    <><p>Product List</p><ul>
+    <><p>Kapil Products List</p><ul>
           {products.map((product) => (
               <li key={product.id}>
                   {product.name} - ${product.price}
