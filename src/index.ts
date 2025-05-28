88159c7f8fc863e33412ce26ed179bfdbcf5a115
kapilelc <58108513+kapilelc@users.noreply.github.com>
2025-05-28 14:14:09 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './components/ProductList';
 export * from './components/FooterSubscribe';
 export * from './components/CookiePreferences';
 export * from './components/StockPriceFooter';
+export * from './components/ContactForm';
 export * from './types/product';
